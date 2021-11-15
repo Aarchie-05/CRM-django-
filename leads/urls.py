8a897e12ffeb7e8f95cdb5f93e77f75d0cca5f2b
Aarchie <aarchie.girdhar10@gmail.com>
2021-11-16 02:45:49 +0530

    Add files via upload

---
+++ leads/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+from .views import (
+    LeadCreateView, 
+    LeadDeleteView, 
+    LeadUpdateView, 
+    lead_Detail, 
+    lead_create, 
+    lead_list, 
+    lead_update, 
+    lead_delete, 
+    LeadListView, 
+    LeadDetailView, 
+    LeadUpdateView, 
+    LeadDeleteView, 
+    AssignAgentView,
+    CategoryListView,
+    CategoryDetailView,
+    LeadCategoryUpdateView,
+    CategoryUpdateView,
+    CategoryDeleteView,
+    CategoryCreateView
+)
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', LeadListView.as_view(), name="lead-list"),
+    path('<int:pk>/', LeadDetailView.as_view(), name="lead-detail"),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name="lead-update"),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name="lead-delete"),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name="assign-agent"), 
+    path('<int:pk>/catrgory/', LeadCategoryUpdateView.as_view(), name="lead-category-update"),
+    path('create/', LeadCreateView.as_view(), name="lead-create"),
+    path('categories/', CategoryListView.as_view(), name="category-list"),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name="category-detail"),
+    path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('create-category/', CategoryCreateView.as_view(), name='category-create'),
+]
